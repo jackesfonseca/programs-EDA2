9f208ce63e9b79554879c219a6592676cdde4f17
jackesfonseca <jackesdafonseca@gmail.com>
2021-08-09 17:25:51 -0300

    small changes on Sanidade e remocao  exercise b

--- 4 - Sanidade e Remoção/MOJ/b.c
+++ 4 - Sanidade e Remoção/MOJ/b.c
@@ -2,20 +2,27 @@
 
 struct Node {
 	unsigned long address;
-	unsigned long before;
-	unsigned long after;
+	unsigned long previous;
+	unsigned long next;
+	int index;
 };
 
 int main(void) {
 	struct Node node[250000];
+	struct Node temp[250000]; //store nodes tha will be deleted
 	int i=0;
 
-    //getting stack
-	while(scanf("%c %c %c ", &node[i].address, &node[i].before, &node[i].after) != EOF){
+  //getting stack
+	/**
+	node[0] has the PTR1 and node[1] has the PTR2
+	*/
+	while(scanf("%c %c %c ", &node[i].address, &node[i].previous, &node[i].next) != EOF)
 		i++;
-	}
 
-  
+	//searching for the position from PTR1 'til PTR2
+
+
+
 
 
 	return 0;
