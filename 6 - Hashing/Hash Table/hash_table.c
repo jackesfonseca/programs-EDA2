8e3ba29ad47b267a9bd6c774e5cc45ebe69eafcf
jackesfonseca <jackesdafonseca@gmail.com>
2021-08-27 11:59:17 -0300

    new functions to hash table

--- 6 - Hashing/Hash Table/hash_table.c
+++ 6 - Hashing/Hash Table/hash_table.c
@@ -1,89 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int hash_n(int n);
-int hash_u(char *hash_table, int tam);
-int hash(int n, int m);
+int hash(int value, int m);
+int hash_u(int tam);
 void hash_init(int *hash_table, int max);
 int hash_count();
-int key(int *hash_table, int index);
+int key(int index);
 int less(int index_a, int index_b);
-int null(int *hash_table, int index);
+int null(int index);
+void hash_insert(int value);
 
 /**
  * m = hash table length
  * n = the amount of elements
- * 262139
+ * 16161
  */
 static int *hash_table;
-static int n=0, m=16161; //prime number to generate hash table for 100000 elements
+static int n=0, m=262139; //prime number to generate hash table for 100000 elements
 
 int main(void)
 {
-	int tam, value, consult, i=0;
-
 	hash_init(hash_table, m);
 
 	//get amount of elements
-	scanf("%d", &tam);
+	
 
 	//finding the hash number values
-	for(i=0; i<tam; i++)
-	{
-		scanf("%d", &value);
-		hash_table[hash_n(value)] = value;
-		n++;
-	}
+	
 
 	//consult if value is on hash table
-	while(scanf("%d", &consult) != EOF)
-	{
-		value = hash_n(consult);	
-		
-		if(hash_table[value] == consult)
-			printf("Found it!\n");
-
-		else if(hash_table[value] == 0)
-			printf("Doesn't exist!\n");
-		
-		else if(hash_table[value] != consult)
-			printf("Colision!\n");
-	}
-
+	
 	return 0;
 }
 
 //hash function
-int hash(int n, int m)
+int hash(int value, int m)
 {
 	//cast to unsigned is to avoid negativy numbers
 	/**
 	 * 618033 is good to random numbers
 	 */
-	return ((16161 * (unsigned) n) % m);
-}
-
-int hash_n(int n)
-{
-    return n % 16161;
+	return ((262139 * (unsigned) value) % m);
 }
 
 //universal hash function
-int hash_u(char *hash_table, int tam)
+int hash_u(int m)
 {
-	int hash, a = 31415, b = 27183;
+	int i, a = 31415, b = 27183;
 
-	for(hash=0; *hash_table != '\n'; hash++)
+	for(i=0; *hash_table != '\n'; i++)
 	{
-		a = a*b % (tam-1);
-		hash = (a*hash + *hash_table) % tam;
+		a = a*b % (m-1);
+		i = (a*i + *hash_table) % m;
 	}
 
-	return hash;
+	return i;
 }
 
 //return hash key element
-int key(int *hash_table, int index)
+int key(int index)
 {
 	return hash_table[index];
 }
@@ -91,15 +66,15 @@ int key(int *hash_table, int index)
 //compare two keys
 int less(int index_a, int index_b)
 {
-	return key(hash_table, index_a) < key(hash_table, index_b);
+	return key(index_a) < key(index_b);
 }
 
 //verify if is a null item
-int null(int *hash_table, int index)
+int null(int index)
 {
 	int null_item = 0;
 
-	return key(hash_table, index) == null_item;
+	return key(index) == null_item;
 }
 
 //initializate the hash table with null
@@ -117,4 +92,35 @@ void hash_init(int *hash_table, int max)
 int hash_count()
 {
 	return n;
+}
+
+//insert item to hash table avoiding colisions
+void hash_insert(int value)
+{
+	int index = hash(value, m);
+
+	//linear probing
+	while(!null(index))
+		index = (index + 1) % m;
+
+	hash_table[index] = value;
+	n++;
+}
+
+//search for element in hash table
+int hash_search(int value)
+{
+	int index = hash(value, m);
+
+	while(!null(index))
+	{
+		if(value == key(index))
+			return hash_table[index];
+
+		else  
+			index = (index + 1) % m;
+	}
+
+	//null item
+	return 0;
 }
