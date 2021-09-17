d7553248c70dd7fd94bba87fd2db1fbfd536a448
jackesfonseca <jackesdafonseca@gmail.com>
2021-09-17 10:17:52 -0300

    working on priority queue

--- 8 - Heap/Fila de prioridade/fila_prioridade.c
+++ 8 - Heap/Fila de prioridade/fila_prioridade.c
@@ -9,9 +9,12 @@ struct pq_st
 
 void init_pq(struct pq_st *PQ, int maxN);
 int empty_pq(struct pq_st *PQ);
-void fix_down();
+void fix_down(struct pq_st *PQ, int k, int pq_len);
 void fix_up(struct pq_st *PQ, int k);
 int less(int ancestor_node, int son_node);
+void exch(struct pq_st *PQ, int son_node, int ancestor_node);
+void insert_pq(struct pq_st *PQ, int item);
+int delMax_pq(struct pq_st *PQ);
 
 int main(void)
 {
@@ -23,7 +26,7 @@ int main(void)
 
 void init_pq(struct pq_st *PQ, int maxN)
 {
-	PQ->pq = malloc(sizeof(int) * (maxN + 1));
+	PQ = malloc(sizeof(int) * (maxN + 1));
 	PQ->n = 0;
 }
 
@@ -34,13 +37,52 @@ int empty_pq(struct pq_st *PQ)
 
 void fix_up(struct pq_st *PQ, int k)
 {
-	while(k>1 && less(PQ[k/2]->pq, PQ[k]->pq))
+	while(k>1 && less(PQ[k/2].pq, PQ[k].pq))
 	{
+		exch(PQ, k, k/2);
+		k = k/2;
+	}
+}
 
+void fix_down(struct pq_st *PQ, int k, int pq_len)
+{
+	int j;
+
+	while(2*k <= pq_len)
+	{
+		j = 2*k;
+		if(j<pq_len && less(PQ[j].pq, PQ[j+1].pq))
+			j++;
+		if(!less(PQ[k].pq, PQ[j].pq))
+			break;
+		exch(PQ, k, j);
+		k = j;
 	}
 }
 
 int less(int ancestor_node, int son_node)
 {
 	return ancestor_node < son_node;
+}
+
+void exch(struct pq_st *PQ, int son_node, int ancestor_node)
+{
+	struct pq_st temp;
+
+	temp = PQ[son_node];
+	PQ[son_node] = PQ[ancestor_node];
+	PQ[ancestor_node] = temp;
+}
+
+void insert_pq(struct pq_st *PQ, int item)
+{
+	PQ[++PQ->n].pq = item;
+	fix_up(PQ, PQ->n); /* fix_up(PQ->, PQ->n); */
+}
+
+int delMax_pq(struct pq_st *PQ)
+{
+	exch(PQ, 1, PQ->n);
+	fix_down(PQ, 1, (PQ->n-1));
+	return PQ[PQ->n].pq;
 }
