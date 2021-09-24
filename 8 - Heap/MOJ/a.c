1d17f92d7193d32f7be6c14fa1bc20f43c843d02
jackesfonseca <jackesdafonseca@gmail.com>
2021-09-24 19:54:37 -0300

    working on Heap exercises

--- 8 - Heap/MOJ/a.c
+++ 8 - Heap/MOJ/a.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Character
 {
@@ -11,9 +12,26 @@ void hash_init(Character *hash_table, int m);
 int main(void)
 {
 	Character *hash_table;
-	int m = 126;
+	int m = 126, cod, i=0;
+	char c;
 
+	/* starting hash table */
+	hash_table = (Character *)malloc(sizeof(Character) * m);
 	hash_init(hash_table, m);
+
+	while(scanf("%d %c", &cod, &c) != EOF)
+	{
+		hash_table[c].cod = cod;
+		hash_table[c].c = c;
+		i++;
+	}
+
+	while(i>=0)
+	{
+		printf("%d %c\n", hash_table[c].cod, hash_table[c].c);
+		i--;
+	}
+
 
 	return 0;
 }
@@ -24,7 +42,7 @@ void hash_init(Character *hash_table, int m)
 
 	for(i=0; i<m; i++)
 	{
-		//hash_table[i].c = "";
+		//hash_table[i].c = " ";
 		hash_table[i].cod = 0;
 	}
 }
